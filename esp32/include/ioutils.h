f04a115dc007057bfc6031d8c87e9c6388917c74
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-02-10 14:58:16 +0700

    more comments

--- esp32/include/ioutils.h
+++ esp32/include/ioutils.h
@@ -1,10 +1,19 @@
 #ifndef IOUTILS_H_
 #define IOUTILS_H_
 
+/**
+ * Load n bytes from the stream into a char arrary and return the pointer to the first element
+ */
 char* loadStream(Stream&, int);
 
+/**
+ * Load the entire content of on file on SPIFFS into a char array and return the pointer to the first element
+ */
 char* loadFile(const char*);
 
+/**
+ * Cycle the pin through HIGH and LOW with delays
+ */
 void ledBlink(int, int, int, int);
 
 #endif  // IOUTILS_H_
